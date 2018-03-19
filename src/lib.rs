14a7b5c631313510f3cb8b08e766049b67cd8bf8
arturo castro <arturo@openframeworks.cc>
2018-03-19 12:05:04 +0100

    added benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(feature = "unstable", feature(test))]
+
 
 use std::mem;
 use std::ptr;
@@ -1277,3 +1279,143 @@ mod test_map {
 
 }
 
+#[cfg(test)]
+mod benches{
+    extern crate test;
+    use self::test::Bencher;
+    use super::DenseVec;
+    use std::collections::HashMap;
+
+
+    #[bench]
+    fn bench_densevec(b: &mut Bencher) {
+        b.iter(||{
+            let mut m = DenseVec::new();
+
+            // Try this a few times to make sure we never screw up the densevec's
+            // internal state.
+            for _ in 0..10 {
+                assert!(m.is_empty());
+
+                for i in 1..1001 {
+                    assert!(m.insert(i, i).is_none());
+
+                    for j in 1..i + 1 {
+                        let r = m.get(j);
+                        assert_eq!(r, Some(&j));
+                    }
+
+                    for j in i + 1..1001 {
+                        let r = m.get(j);
+                        assert_eq!(r, None);
+                    }
+                }
+
+                for i in 1001..2001 {
+                    assert!(!m.contains_key(i));
+                }
+
+                // remove forwards
+                for i in 1..1001 {
+                    assert!(m.remove(i).is_some());
+
+                    for j in 1..i + 1 {
+                        assert!(!m.contains_key(j));
+                    }
+
+                    for j in i + 1..1001 {
+                        assert!(m.contains_key(j));
+                    }
+                }
+
+                for i in 1..1001 {
+                    assert!(!m.contains_key(i));
+                }
+
+                for i in 1..1001 {
+                    assert!(m.insert(i, i).is_none());
+                }
+
+                // remove backwards
+                for i in (1..1001).rev() {
+                    assert!(m.remove(i).is_some());
+
+                    for j in i..1001 {
+                        assert!(!m.contains_key(j));
+                    }
+
+                    for j in 1..i {
+                        assert!(m.contains_key(j));
+                    }
+                }
+            }
+        });
+    }
+
+
+
+    #[bench]
+    fn bench_hashmap(b: &mut Bencher) {
+        b.iter(||{
+            let mut m = HashMap::new();
+
+            // Try this a few times to make sure we never screw up the densevec's
+            // internal state.
+            for _ in 0..10 {
+                assert!(m.is_empty());
+
+                for i in 1..1001 {
+                    assert!(m.insert(i, i).is_none());
+
+                    for j in 1..i + 1 {
+                        let r = m.get(&j);
+                        assert_eq!(r, Some(&j));
+                    }
+
+                    for j in i + 1..1001 {
+                        let r = m.get(&j);
+                        assert_eq!(r, None);
+                    }
+                }
+
+                for i in 1001..2001 {
+                    assert!(!m.contains_key(&i));
+                }
+
+                // remove forwards
+                for i in 1..1001 {
+                    assert!(m.remove(&i).is_some());
+
+                    for j in 1..i + 1 {
+                        assert!(!m.contains_key(&j));
+                    }
+
+                    for j in i + 1..1001 {
+                        assert!(m.contains_key(&j));
+                    }
+                }
+
+                for i in 1..1001 {
+                    assert!(!m.contains_key(&i));
+                }
+
+                for i in 1..1001 {
+                    assert!(m.insert(i, i).is_none());
+                }
+
+                // remove backwards
+                for i in (1..1001).rev() {
+                    assert!(m.remove(&i).is_some());
+
+                    for j in i..1001 {
+                        assert!(!m.contains_key(&j));
+                    }
+
+                    for j in 1..i {
+                        assert!(m.contains_key(&j));
+                    }
+                }
+            }
+        });
+    }
+}
